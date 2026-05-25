fd6f1c222dfa9fbaaa0f98521abbe05ef90e846a
naridnevahgar <naridnevahgar@gmail.com>
2026-05-25 12:43:42 +0900

    org change for cors

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:4200",
-        "https://naridnevahgar.github.io",
+        "https://macronalyze.github.io",
     ],
     allow_credentials=True,
     allow_methods=["*"],
